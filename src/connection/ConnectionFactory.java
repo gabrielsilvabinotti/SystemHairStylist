4d7c9a660386dd439fcf257ff72311c708494d2c
Gabriel Binotti <gabriel.binotti1992@gmail.com>
2017-07-05 15:00:09 -0300

    System Hair Stylist V1

---
+++ src/connection/ConnectionFactory.java
@@ -0,0 +1,66 @@
+package connection;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ * @author Gabriel
+ */
+public class ConnectionFactory {
+    private static final String DRIVER  = "com.mysql.jdbc.Driver";
+    private static final String URL     = "jdbc:mysqli//localhost:3306/systemhairstylist";
+    private static final String USER    = "root";
+    private static final String PASS    = "";
+    
+    public static Connection getConnection(){
+        try{
+            Class.forName(DRIVER);
+            return DriverManager.getConnection(URL,USER,PASS);
+        }catch(ClassNotFoundException | SQLException erro){
+            throw new RuntimeException("Erro de conexão: ", erro);
+        }
+    }
+    
+    public static void closeConnection(Connection con) {
+        try {
+            if (con != null) {
+                con.close();
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(ConnectionFactory.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    public static void closeConnection(Connection con, PreparedStatement stmt) {
+
+        closeConnection(con);
+
+        try {
+            if (stmt != null) {
+                stmt.close();
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(ConnectionFactory.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    
+    public static void closeConnection(Connection con, PreparedStatement stmt, ResultSet rs) {
+
+        closeConnection(con, stmt);
+
+        try {
+
+            if (rs != null) {
+                rs.close();
+            }
+
+        } catch (SQLException ex) {
+            Logger.getLogger(ConnectionFactory.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+}
